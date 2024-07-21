b233cbf47e7ed90478f12399244b4adf3b21adb3
programORdie2 <contact.programordie@gmail.com>
2024-07-21 15:00:12 +0200

    make sure paths exist

--- src/services/imageDatabase.ts
+++ src/services/imageDatabase.ts
@@ -1,6 +1,19 @@
-import { promises } from "fs";
+import { promises, existsSync, mkdirSync } from "fs";
 import sharp from "sharp";
 import { UPLOAD_PATH } from "../config";
+import logger from "../utils/logger.util";
+
+if (!existsSync(UPLOAD_PATH)) {
+    mkdirSync(UPLOAD_PATH);
+}
+
+if (!existsSync(UPLOAD_PATH + "/avatars")) {
+    mkdirSync(UPLOAD_PATH + "/avatars");
+}
+
+if (!existsSync(UPLOAD_PATH + "/avatars/defaults")) {
+    mkdirSync(UPLOAD_PATH + "/avatars/defaults");
+}
 
 async function _resizeImage(base64: string, width: number, height: number) {
     const buffer = Buffer.from(base64, 'base64');
@@ -30,6 +43,11 @@ async function uploadDefaultAvater(imagePath: string) {
     const randomIndex = Math.floor(Math.random() * defaultAvatars.length);
     const defaultAvatar = defaultAvatars[randomIndex];
 
+    if (!existsSync(UPLOAD_PATH + defaultAvatar)) {
+        logger.critical("Default avatar not found: " + defaultAvatar);
+        process.exit(1);
+        return;
+    }
     const base64 = await promises.readFile(UPLOAD_PATH + defaultAvatar, 'base64');
 
     return await uploadAvater(imagePath, base64);
