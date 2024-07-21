c8c386af4cb490a6ddcdd91123f456a525c04cd6
programORdie2 <contact.programordie@gmail.com>
2024-07-21 10:37:06 +0200

    Improved user profile

--- src/services/userProfileLoader.ts
+++ src/services/userProfileLoader.ts
@@ -3,5 +3,5 @@ import {User} from "../models/user.model";
 export async function loadUserProfile(username: string) {
     const user = await User.findOne({ username_lowercase: username.toLowerCase() });
     if (!user) return null;
-    return { username: user.username, createdAt: user.createdAt, avatar: user.avatar };
+    return { username: user.username, createdAt: user.createdAt, avatar: user.avatar, pronounce: user.pronounce, bio: user.bio, displayName: user.displayName };
 }
