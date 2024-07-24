a4d76dc394acaac8ce98ee917676a84ee85c1eb1
programORdie2 <contact.programordie@gmail.com>
2024-07-24 23:38:26 +0200

    api error handling

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -7,5 +7,10 @@ import logger from "../utils/logger.util";
 export default async (err: any, req: CustomRequest, res: Response, next: NextFunction) => {
     logger.error(err);
 
+    if (req.path.startsWith("/api/")) {
+        res.status(500).json({ succes: false, message: "Internal server error" });
+        return;
+    }
+    
     send500page(req, res);
 }
