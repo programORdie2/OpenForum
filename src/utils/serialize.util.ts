11023108cd498775f886cc8a37e7bfc5625503ff
programORdie2 <contact.programordie@gmail.com>
2024-07-24 16:46:06 +0200

    string serialization update

--- src/utils/serialize.util.ts
+++ src/utils/serialize.util.ts
@@ -1,17 +1,20 @@
 function serialize(text: string) {
-    const entitiyMap: { [key: string]: string } = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#39;',
-        '/': '&#x2F;',
-        '`': '&#x60;',
-        '=': '&#x3D;',
-    }
+    // const entitiyMap: { [key: string]: string } = {
+    //     '&': '&amp;',
+    //     '<': '&lt;',
+    //     '>': '&gt;',
+    //     '"': '&quot;',
+    //     "'": '&#39;',
+    //     '/': '&#x2F;',
+    //     '`': '&#x60;',
+    //     '=': '&#x3D;',
+    // }
 
-    const regex = /[&<>"'/`=]/g
-    return text.replace(regex, (s) => entitiyMap[s])
+    // const regex = /[&<>"'/`=]/g
+    // return text.replace(regex, (s) => entitiyMap[s])
+
+    // EJS escapes it anyway
+    return text
 }
 
 export default serialize
